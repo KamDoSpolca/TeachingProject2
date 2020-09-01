5171d404a66f2e4ffe0d93562b1d289677a083bc
martin pastor <martin.pastor@ibm.com>
2020-09-01 20:07:36 +0200

    1.september - basic-routing

---
+++ src/app/photos/photos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'photos',
+  templateUrl: './photos.component.html',
+  styleUrls: ['./photos.component.scss']
+})
+
+export class PhotosComponent {
+
+}
